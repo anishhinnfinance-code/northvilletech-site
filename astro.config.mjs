58d5c9d48df9ab9160a2ef724216ad05e9dbdfdf
anishhinnfinance-code <anishhinnfinance@gmail.com>
2026-04-21 15:23:54 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://northvilletech.com',
-  integrations: [sitemap()],
-  markdown: {
-    shikiConfig: {
-      theme: 'github-dark',
-      wrap: true,
+    site: 'https://northvilletech.com',
+    markdown: {
+          shikiConfig: {
+                  theme: 'github-dark',
+                  wrap: true,
+          },
     },
-  },
 });
